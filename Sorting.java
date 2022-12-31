7b8c0949ff4e091d562aea2a1627cf06b1b3167d
kumaramarjeet7503 <amarjeet1998@outlook.com>
2022-12-31 18:43:06 +0530

    sorting techniques like bubble sort , selection sort and insertion sort

---
+++ Sorting.java
@@ -0,0 +1,73 @@
+package MyProject;
+
+import java.util.Iterator;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		int num[] = new int[] {12,21,10,12,15,3};
+		
+		int sortedArray[] = new int[num.length] ;
+//		sortedArray = bubbleSort(num);
+//		sortedArray = selectionSort(num);
+		sortedArray = insertionSort(num);
+
+		printArray(sortedArray);
+	}
+	
+	public static void printArray(int[] array)
+	{
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+	
+	public static int[] bubbleSort(int[] array) 
+	{
+			for (int i = 0; i < array.length-1; i++) {
+			for (int j = i+1; j < array.length; j++) {
+				if(array[i] > array[j]) {
+					swap(array,i,j);				}
+			}
+		}
+		
+		return array;
+	}
+	
+	public static int[] selectionSort(int[] array) 
+	{
+		for (int i = 0; i < array.length; i++) {
+			int min = i;
+			for (int j = i+1 ; j < array.length; j++) {
+				if(array[j] < array[i] ) {
+					min = j;
+				}
+				swap(array,i,min);
+			}
+		}
+		return array;
+	}
+	
+	public static int[] insertionSort(int[] array) 
+	{
+		for (int i = 0; i < array.length; i++) {
+			int current = array[i];
+			int j = i-1 ;
+			while(j >= 0 && current < array[j]  ) 
+			{
+				array[j+1] = array[j];
+				j-- ;
+			}
+			array[j+1] = current ;
+		}
+		return array;
+	}
+	
+	public static int[] swap(int[] array , int a , int b) {
+		int swap = array[b];
+		array[b] = array[a];
+		array[a] = swap ;
+		return array ;
+	}
+
+}
