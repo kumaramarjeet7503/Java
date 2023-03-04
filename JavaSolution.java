5193fa4e767f12e38aff1ee8d0ad522f02a46611
kumaramarjeet7503 <amarjeet1998@outlook.com>
2023-03-04 18:26:37 +0530

    added function for hcf and lcm

--- JavaSolution.java
+++ JavaSolution.java
@@ -12,7 +12,15 @@ public class JavaSolution {
 		
 //		isPalindrome(name);
 //		findAmstrongNumber(153,1000);
-		printFactors(10);
+//		printFactors(10);
+//		 int lcm = calculateLCM(20,30);
+//		int nums[] = {2,2,4};
+//		int output[] =  twoSum(nums, 4);
+//		System.out.println(output);
+//		System.out.println(lcm);
+		
+		int hcf = calculateHCF(10,5) ;
+		System.out.println(hcf);
 		
 		
 	}
@@ -102,4 +110,60 @@ public class JavaSolution {
     		i++;
     	}
     }
+
+    public static int calculateLCM(int firstNumber, int secondNumber) 
+    {
+    	int i = 2 ;
+    	int max = firstNumber > secondNumber ? firstNumber : secondNumber ;
+    	while(true) 
+    	{
+    		if(firstNumber % max == 0  && secondNumber % max == 0  ) 
+    		{  
+    			break;	
+    		}
+    		i++ ;
+    	}
+    	
+    	return i;
+    }
+    
+    public static int calculateHCF(int num1, int num2) 
+    {
+    	int i = 2;
+    	int min  = num1 > num2 ? num2 : num1 ;
+    	int hcf = 1;
+    	while(i <= min) 
+    	{
+    		if(num1 % i == 0 && num2 % i == 0) 
+    		{
+    			hcf = hcf * i ;
+    			num1 = num1/i ;
+    			num2 = num2/i ;
+    			min  = num1 > num2 ? num2 : num1 ;
+    			i = 2;
+    		}else 
+    		{
+    			i++ ;
+    		}
+    		
+    	}
+    	
+    	return hcf ;
+    }
+    
+//    public static int[]  twoSum(int[] nums, int target) {
+//        int i = 0;
+//        int output[] = null; 
+//        for(i=0; i<nums.length-1; i++)
+//        {
+//            for(int j=i+1; j<nums.length; j++)
+//            {
+//                if(nums[i] + nums[j] == target )
+//                {
+//                    output = {i,j};
+//                    return output ;
+//                }
+//            }
+//        }
+//    }
 }
